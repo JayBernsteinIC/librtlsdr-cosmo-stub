fcbe372fc416ab05984abd3c82dc0f8c98a11479
Jay Bernstein <jay.bernstein@myinstantcare.com>
2025-02-28 16:15:15 -0800

    Highly annoying, same issue for other shell scripts

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/sh -e
 # sudo sh -c 'echo -1 > /proc/sys/fs/binfmt_misc/WSLInterop'
 sed -i 's/\r//' build.sh #git is annoying, you need to destroy carriage returns to make this shell script parse correctly in unix
+sed -i 's/\r//' deps/build_libusb_stubs.sh #git is annoying, you need to destroy carriage returns to make this shell script parse correctly in unix
 
 BASE_DIR=$PWD
-
-
 
 if ! command -v cosmocc > /dev/null; then
   export PATH=$PATH:/home/$USER/cosmocc/bin
