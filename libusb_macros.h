22f933796f1437f56c58d25565e4e0618ee22244
Jay Bernstein <jay.bernstein@myinstantcare.com>
2025-07-16 10:52:44 -0700

    Cleanup and using Libusb 1.0.27

--- libusb_macros.h
+++ libusb_macros.h
@@ -1,3 +1,6 @@
-#define LIBUSB_DEBUG LIBUSB_LOG_LEVEL_DEBUG //Blood sweat and tears to find this lol
-#define ENABLE_DEBUG_LOGGING
+//#define LIBUSB_DEBUG LIBUSB_LOG_LEVEL_DEBUG //Blood sweat and tears to find this lol
+#ifdef ENABLE_DEBUG_LOGGING
+#undef ENABLE_DEBUG_LOGGING
+#endif
+//#define ENABLE_DEBUG_LOGGING
 
